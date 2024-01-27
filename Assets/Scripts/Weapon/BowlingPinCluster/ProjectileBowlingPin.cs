f39ff4f1d83be584cb6de878746eb850fa09f302
zanecosmo <zanecosmo@gmail.com>
2024-01-27 18:37:09 -0500

    balloon projectile sounds and refactors

--- Assets/Scripts/Weapon/BowlingPinCluster/ProjectileBowlingPin.cs
+++ Assets/Scripts/Weapon/BowlingPinCluster/ProjectileBowlingPin.cs
@@ -2,31 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BowlingPinProjectile : MonoBehaviour
+public class BowlingPinProjectile : Projectile
 {
     [SerializeField]
     private GameObject explosionAnimation;
 
-    bool exploded = false;
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!exploded && collision.gameObject.CompareTag("Terrain"))
+        if (collision.gameObject.CompareTag("Terrain"))
         {
             GetComponent<Animator>().SetTrigger("Explode");
             GetComponent<AudioSource>().Play();
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Rigidbody2D>().gravityScale = 0;
+            GetComponent<Collider2D>().enabled = false;
 
-            exploded = true;
         }
-    }
-
-    public void BreakTerrain()
-    {
-        GetComponent<TerrainBreaker>().BreakTerrain();
-    }
-
-    public void DestroySelf()
-    {
-        Destroy(this.gameObject);
     }
 }
