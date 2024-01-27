98e134fd0cf69e15e009ea5c07e9dfeb6b85a71c
Simon Post <simon.post.1999@gmail.com>
2024-01-27 01:12:17 -0500

    perlin noise looks pretty good

--- Assets/Scripts/NoiseManager.cs
+++ Assets/Scripts/NoiseManager.cs
@@ -8,18 +8,28 @@ public class NoiseManager : MonoBehaviour
 {
     public RawImage noiseTextureImage;
 
-    public int width = 256;
-    public int height = 256;
-    public float scale = .15f;
+    public int width = 10000;
+    public int height = 10000;
+    public float smoothness = 10000;
 
     private void Awake()
     {
+        var seed = Random.Range(0, 1000000);
         float[,] noise = new float[width, height];
-        for (int y = 0; y < height; y++)
+        for (int x = 0; x < width; x++)
         {
-            for (int x = 0; x < width; x++)
+            var hillHeight = Mathf.RoundToInt(height * Mathf.PerlinNoise(x / smoothness + seed, 0));
+
+            for (int y = 0; y < height; y++)
             {
-                noise[x, y] = Mathf.PerlinNoise(x * scale, y * scale);
+                if (y < hillHeight)
+                {
+                    noise[x, y] = 1;
+                }
+                else
+                {
+                    noise[x, y] = 0;
+                }
             }
         }
         _SetNoiseTexture(noise);
