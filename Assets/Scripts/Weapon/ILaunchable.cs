f729e848519c12c224dca7cb227e6b3e044cc26c
Abraham Post <abrahampost@yahoo.com>
2024-01-26 22:00:19 -0500

    Initial balloon launcher

---
+++ Assets/Scripts/Weapon/ILaunchable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Launchable : MonoBehaviour {
+    public abstract void Launch(float angle, float power);
+
+}
