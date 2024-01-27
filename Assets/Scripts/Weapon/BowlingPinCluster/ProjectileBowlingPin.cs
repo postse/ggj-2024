706b7b2ac59b0e2f021fc1892c21791bcd05db7d
zanecosmo <zanecosmo@gmail.com>
2024-01-27 17:56:07 -0500

    added bowling pin sound

--- Assets/Scripts/Weapon/BowlingPinCluster/ProjectileBowlingPin.cs
+++ Assets/Scripts/Weapon/BowlingPinCluster/ProjectileBowlingPin.cs
@@ -7,29 +7,26 @@ public class BowlingPinProjectile : MonoBehaviour
     [SerializeField]
     private GameObject explosionAnimation;
 
-    [SerializeField]
-    private AudioSource _audioSource;
-
     bool exploded = false;
-
-    void Start()
-    {
-        _audioSource = GetComponent<AudioSource>();
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (!exploded && collision.gameObject.CompareTag("Terrain"))
         {
-            Debug.Log(_audioSource);
-            _audioSource.Play();
+            GetComponent<Animator>().SetTrigger("Explode");
+            GetComponent<AudioSource>().Play();
 
             exploded = true;
-            
-            GameObject anim = Instantiate(explosionAnimation, this.transform.position, Quaternion.identity);
-            // TODO: Don't hardcode explosion time
-            Destroy(anim, .6f);
-            Destroy(this.gameObject, 1);
         }
+    }
+
+    public void BreakTerrain()
+    {
+        GetComponent<TerrainBreaker>().BreakTerrain();
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(this.gameObject);
     }
 }
