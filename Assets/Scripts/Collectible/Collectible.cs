f01e124095a9af6a3173af0de255712001962166
Denver Massey <denmas22@live.unc.edu>
2024-01-28 10:56:22 -0500

    removed debug messages

--- Assets/Scripts/Collectible/Collectible.cs
+++ Assets/Scripts/Collectible/Collectible.cs
@@ -6,7 +6,6 @@ public class Collectible : MonoBehaviour, ICollectible
 {
     void OnTriggerEnter2D(Collider2D otherCollider) {
         if (otherCollider.gameObject.tag == "CarBody") {
-            Debug.Log("Hit player!");
             PickUp(otherCollider.gameObject.transform.parent.gameObject);
         } else if (otherCollider.gameObject.tag == "Terrain") {
             GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
