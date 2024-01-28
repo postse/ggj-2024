126e8ac1b2352d720d97ececb3e0da33004a3d9c
Simon Post <simon.post.1999@gmail.com>
2024-01-27 23:33:29 -0500

    camera inset

--- Assets/Scripts/DTerrain_Scripts/ExampleScripts/CameraMovement.cs
+++ Assets/Scripts/DTerrain_Scripts/ExampleScripts/CameraMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    [SerializeField]
+    private float cameraInset = 2f;
+
     void Start()
     {
         var parent = GetComponentInParent<TerrainGenerator>();
@@ -13,11 +16,11 @@ public class CameraMovement : MonoBehaviour
         var terrainPosition = parent.transform.position;
 
         // Calculate the orthographic size based on the desired width and the aspect ratio
-        Camera.main.orthographicSize = desiredWidth / (2 * Camera.main.aspect);
+        Camera.main.orthographicSize = desiredWidth / (2 * Camera.main.aspect) - (cameraInset / 2);
 
         // Position the camera so that the bottom left of the view is at (0,0)
         float cameraX = Camera.main.orthographicSize * Camera.main.aspect; // half of the horizontal viewing area
-        Camera.main.transform.position = new Vector3(terrainPosition.x + cameraX, terrainPosition.y + Camera.main.orthographicSize, -10); // Adjust the Z value as needed
+        Camera.main.transform.position = new Vector3(terrainPosition.x + cameraX + cameraInset, terrainPosition.y + Camera.main.orthographicSize + cameraInset, -10); // Adjust the Z value as needed
         
         ogPosition = transform.localPosition;
     }
