a6c25e284eee838e03d45982a3de1185d41b66be
Simon Post <simon.post.1999@gmail.com>
2024-01-28 01:43:05 -0500

    only show power/aim when it's your turn

---
+++ Assets/ShowOnTurn.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowOnTurn : MonoBehaviour
+{
+    private CarController carController;
+    private Vector3 startingScale;
+
+    void Start()
+    {
+        carController = GetComponentInParent<CarController>();
+        startingScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        if (carController.isTurn)
+        {
+            transform.localScale = startingScale;
+        }
+        else
+        {
+            transform.localScale = Vector2.zero;
+        }
+    }
+}
